197ec03a8b8758aa4efb654b124c661bc57a9394
Ruwan Egodawatte <617196+ruwanego@users.noreply.github.com>
2020-05-27 14:36:29 +0530

    All Excercises

---
+++ variables/variables5.rs
@@ -0,0 +1,9 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
+
+fn main() {
+    let mut number = "3";
+    println!("Number {}", number);
+    number = "5";
+    println!("Number {}", number);
+}
