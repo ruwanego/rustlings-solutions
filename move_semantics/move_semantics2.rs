427f8a9b7887f48cda90d6b61a3bc3adf4a73151
Ruwan Egodawatte <617196+ruwanego@users.noreply.github.com>
2020-05-27 17:03:31 +0530

    mut keyword

--- move_semantics/move_semantics2.rs
+++ move_semantics/move_semantics2.rs
@@ -9,6 +9,9 @@ fn main() {
 
     let mut vec1 = fill_vec(vec0);
 
+    let vec0n = &Vec::new();
+    let mut vec1 = fill_vec(vec0);
+
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
